d21bf9df1dee558695dc9521709ff7dd24eb4245
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2024-10-04 20:34:49 -0300

    arruma bug navbar

--- static/Resultados.js
+++ static/Resultados.js
@@ -1,3 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+        const cortinaLeft = document.querySelector('.cortina-left');
+        const cortinaRight = document.querySelector('.cortina-right');
+        
+        // Adiciona a classe de animação
+        setTimeout(() => {
+            cortinaLeft.classList.add('animate');
+            cortinaRight.classList.add('animate');
+        }, 1000); // Ajuste o tempo de acordo com a necessidade
+    
+        // Detecta o final da animação de transição
+        cortinaLeft.addEventListener('transitionend', function() {
+            cortinaLeft.parentElement.classList.add('hidden');
+        });
+        
+        cortinaRight.addEventListener('transitionend', function() {
+            cortinaRight.parentElement.classList.add('hidden');
+        });
+    });
 var bits=400; // Número de pontos
         var intensity=20; // Intensidade da explosao (recomendado entre 3 e 10)
         var speed=1; // Velocidade (qnto menor for o numero, mais rapido)
